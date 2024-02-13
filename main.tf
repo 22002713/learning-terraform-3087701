14143d5e4ef792e1c220bf2c7501ad3b607a3df7
Jonathan Eduard Lim <99624429+22002713@users.noreply.github.com>
2024-02-13 21:48:41 +0800

    Use Module for Security Group

--- main.tf
+++ main.tf
@@ -23,18 +23,38 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  # Use the below created module to put below
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  # The explanation of this code is defined in the Input section of the README
+  # Link: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest?tab=inputs
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+
+}
+
+
 resource "aws_security_group" "blog" {
   # Specify the name here, if not Terraform will randomly generate each run 
-  name = "blog"
+  name        = "blog"
   description = "Allow http and https in. Allow everything out"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id      = data.aws_vpc.default.id
 }
 
 # Defining an intuitive name here will be good to briefly describe what's it for 
