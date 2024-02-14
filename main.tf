a5e4a65990efcc45316a648cce4d219d9ac68408
Jonathan Eduard Lim <99624429+22002713@users.noreply.github.com>
2024-02-14 22:13:07 +0800

    try to fix the error..

--- main.tf
+++ main.tf
@@ -36,23 +36,20 @@ module "blog_vpc" {
 }
 
 # This will replace the about resource instance created earlier  
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.3.1"
 
-
   # insert the 1 required variable here
   name     = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-
-  security_groups = [module.blog_sg.security_group_id]
-  image_id        = data.aws_ami.app_ami.id
-  instance_type   = var.instance_type
-
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
