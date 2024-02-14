af6c9c0cb03b6465f45fe7258dd28f71c3ab0156
Jonathan Eduard Lim <99624429+22002713@users.noreply.github.com>
2024-02-14 21:56:49 +0800

    autoscaling

--- main.tf
+++ main.tf
@@ -35,18 +35,24 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+# This will replace the about resource instance created earlier  
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
 
-  # Use the below created module to put below
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  # insert the 1 required variable here
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+
+  security_groups = [module.blog_sg.security_group_id]
+  image_id        = data.aws_ami.app_ami.id
+  instance_type   = var.instance_type
+
 }
 
 module "blog_alb" {
