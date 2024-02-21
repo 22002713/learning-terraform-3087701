8501ab787a011053220e2575dfa3377e94cba3f8
Jonathan Eduard Lim <99624429+22002713@users.noreply.github.com>
2024-02-21 22:06:22 +0800

    cleaned up variable.tf

--- variables.tf
+++ variables.tf
@@ -19,27 +19,18 @@ type = object({
   }
 }
 
-  variable "environment" {
-    description = "Development Environment"
+variable "environment" {
+  description = "Development Environment"
 
-    type = object({
-      name           = string
-      network_prefix = string 
-    })
+  type = object({
+    name           = string
+    network_prefix = string 
+  })
 
-    default = {     
-    name           = "dev"
-    network_prefix = "10.0"
-    }
-      
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  default = {     
+  name           = "dev"
+  network_prefix = "10.0"
+  }      
 }
 
 variable "asg_min_size" {
