d9303fc8419299a71c85cd318a81fd8c5d95786e
Jonathan Eduard Lim <99624429+22002713@users.noreply.github.com>
2024-02-24 12:46:20 +0800

    initial commit for Prod Environment

---
+++ prod/main.tf
@@ -0,0 +1,11 @@
+module "qa" {
+    source = "../modules/blog"
+
+    environment = {
+        name           = "prod"
+        network_prefix = "10.1"
+    }
+    # Save some charges from AWS account
+    asg_min_size = 1
+    asg_max_size = 1
+}
